f6c74ab0229af9893b83b38c4faca55e2fea6b2b
andershup <anders242@gmail.com>
2020-10-04 12:54:53 +0000

    Static and media files added

--- usupplement/settings.py
+++ usupplement/settings.py
@@ -150,3 +150,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  
+#telling django where our static files are. BSE_DIR because they in project level static foler
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# where uploaded media files will go
+
