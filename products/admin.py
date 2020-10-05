3d25d82d504cfaad91b0c5ef4fd3695fe3910f78
andershup <anders242@gmail.com>
2020-10-05 08:41:40 +0000

    Admin columns setup in admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
 from .models import Product, Category
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+
+    """Class which will extend the built in model admin class"""
+    list_display = (
+    # List display attribute as a tuple to tell admin which fields to display
+    # Column order in admin can be changed here
+        'name',
+        'category',
+        'sku',
+        'price',
+        'rating',
+        'image',
+    )
+    # Ordering attribute tuple to sort by 'sku'
+    ordering = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """Class which will extend the built in model admin class"""
+    list_display = (
+    # List display attribute as a tuple to tell admin which fields to display
+        'name',
+        'friendly_name',
+    )
+
+# Category and Classes registration
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
