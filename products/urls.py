36233b433b089ec389b5860fb5acc8acfeb93783
andershup <anders242@gmail.com>
2020-10-13 12:30:23 +0000

    Added add_product template

--- products/urls.py
+++ products/urls.py
@@ -5,6 +5,10 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>', views.product_detail, name='product_detail'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'), #the / is just good practice
+    # we have had to update the above with a int: to specidy id should be an int since otherwise navigation to products /add will interpret the 
+    # string add as a product id because django will always the first URL it finds a matching pattern for and in this case unless we specify that product id is
+    # an integer django does not know the fifference between a product number an da string like add
     # note contains the product id and named product_detail not product
+    path('add/', views.add_product, name='add_product'),
 ]
