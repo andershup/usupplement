75982d1fec2283894526a141a54f89096494e92b
andershup <anders242@gmail.com>
2020-10-13 11:54:43 +0000

    Added product admin form

---
+++ products/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Product, Category
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__' # dunder that will include all the fields
+
+    def __init__(self, *args, **kwargs): # we override the init method to make a few changes to the field
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all() #get all the categories
+        #Create a list of tuples associated with their category ids
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories] # list comprehension syntax
+        # use friendly names instead of id
+        self.fields['category'].choices = friendly_names
+        #itterate and add classes to match the rest of the store
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
