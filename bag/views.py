8d6012c2a330829d10b57dc67b9b4c6d22c52d62
andershup <anders242@gmail.com>
2020-10-20 12:41:30 +0000

    Toasts templates added and functional

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, HttpResponse
-
+from django.contrib import messages
+from products.models import Product
 # Create your views here.
 
 def view_bag(request):
@@ -10,23 +11,28 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    product = Product.objects.get(pk=item_id) #For showing message "success" below.
     quantity = int(request.POST.get('quantity')) # First thing is to get the quantity from the form. int cause it comes as a string.
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Added {product.name} to your bag')
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
 
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
+    product = Product.objects.get(pk=item_id)
     bag = request.session.get('bag', {})
     try:
         bag.pop(item_id)
+        messages.info(request, f'Removed {product.name} from your bag')  
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
