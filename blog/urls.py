0bd71f2ba29bfa31a2716b8c1b43185ba8b3cdac
andershup <anders242@gmail.com>
2020-10-23 08:55:03 +0000

    Adding blog template added and submit tested ok

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='blog'),
     path('post/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('add_blog/', views.add_blog , name='add_blog'),
+    path('add/', views.add_blog , name='add_blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.views import generic
 from .models import Post
+from .forms import BlogForm 
+
+@login_required
+def add_blog(request):
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added your blog!')
+            return redirect(reverse('add_blog'))
+        else: 
+            messages.error(request, 'UUPPPS try that again')
+    else:
+        form = BlogForm()
+    template = 'blog/add_blog.html'
+    context = {
+        'form' : form,
+    }
+    return render(request, template, context)
 
 class PostList(generic.ListView): #the built in listview.
     queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'post.html'
+    template_name = 'blog/post.html'
 
 
 
 class PostDetail(generic.DetailView):
     model = Post
-    template_name = 'post_detail.html'
+    template_name = 'blog/post_detail.html'
 
-@login_required
-def add_blog(request):
-    return render(request, 'blog/add_blog.html')
