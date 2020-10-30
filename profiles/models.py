25f58d1a1cecd6f3c67f46c11c27df9224f37f79
andershup <anders242@gmail.com>
2020-10-30 10:05:19 +0000

    All code validated and formatted

--- profiles/models.py
+++ profiles/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save #used in the reciever
-from django.dispatch import receiver #For the signal to work
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django_countries.fields import CountryField
 
 # Create your models here.
+
 
 class UserProfile(models.Model):
     """
@@ -13,8 +13,6 @@ class UserProfile(models.Model):
     delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # delivery fields we want the user to be able to provide defaults
-    # these can come directly from the order model and we want all fields to be optional
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
@@ -27,14 +25,14 @@ class UserProfile(models.Model):
         return self.user.username
 
 
-
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
-    Adding the receiver for the post save envent from the user model. Since there is only one signal we are not putting it in a seperate signals.py module like 
+    Adding the receiver for the post save envent from the user model.
+    Since there is only one signal we are not putting it in a seperate signals.py module like
     we did with the other ones in the order model
     """
     if created:
         UserProfile.objects.create(user=instance)
-    #Existing users: just save the profile
+    # Existing users: just save the profile
     instance.userprofile.save()
--- profiles/views.py
+++ profiles/views.py
@@ -1,28 +1,30 @@
 from django.shortcuts import render, get_object_or_404
-from django.contrib import messages #You dont have messages
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-
 from .models import UserProfile
 from .forms import UserProfileForm
 
 # Create your views here.
+# only logged in users should be able to view this view
 
-@login_required # only logged in users should be able to view this view
+
+@login_required
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
-        """creat and instance of the user profile form using post data. Ant tell it the instance we are updating is the profile we have 
+        """creat and instance of the user profile form using post data.
+        tell it the instance we are updating is the profile we have
         just retrieved above. If form valid we save and add success message"""
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
-            #this needs to be changed because you dont have messages
             messages.success(request, 'Profile updated successfully')
-        else: 
+        else:
             message.error(request, 'Update failed. Please insure the form is valid.')
-    else: #we move the form instantation into an else block so it wont wipe out the form errors
+    # we move the form instantation into an else block so it wont wipe out the form errors
+    else:
         form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
@@ -33,6 +35,7 @@ def profile(request):
     }
     return render(request, template, context)
 
+
 def order_history(request, order_number):
         order = get_object_or_404(Order, order_number=order_number)
 
@@ -41,10 +44,11 @@ def order_history(request, order_number):
             'A confirmation email was sent on the order date.'
         ))
 
-        template = 'checkout/checkout_success.html' #same template as checkout
+        template = 'checkout/checkout_success.html'
         context = {
             'order': order,
-            'from_profile': True, #Variable to check if the user got there via the order history
-        }    
+            # Check if the user got there via the order history
+            'from_profile': True,
+        }
 
         return render(request, template, context)
